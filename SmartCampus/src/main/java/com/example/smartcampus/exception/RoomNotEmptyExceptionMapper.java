d33724851cfe24777188a3c9bb80bb4b60aa7391
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-04-23 00:06:56 +0530

    Add the RoomNotEmptyExceptionMapper

---
+++ SmartCampus/src/main/java/com/example/smartcampus/exception/RoomNotEmptyExceptionMapper.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.smartcampus.exception;
+
+import com.example.smartcampus.model.ErrorMessage;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author Isith Bhanuka
+ */
+
+@Provider
+public class RoomNotEmptyExceptionMapper implements
+        ExceptionMapper<RoomNotEmptyException>{
+    
+    @Override
+    public Response toResponse(RoomNotEmptyException exception) {
+        
+        // Create an instance of ErrorMessage class
+        ErrorMessage errorMessage = new ErrorMessage(
+            exception.getMessage(),
+            404,
+            "Room is not empty. Cannot delete the room!");
+        
+        // Build the response entity
+        return Response.status(Response.Status.CONFLICT)
+                .entity(errorMessage)
+                .build();
+        
+    }
+    
+}
