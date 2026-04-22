e76f6c83e92d635f30ac458cc2cb43cc21ec1320
Isith Bhanuka <isithbhanuka2006@gmail.com>
2026-04-22 22:23:03 +0530

    Create the a mock database to work with

---
+++ SmartCampus/src/main/java/com/example/smartcampus/dao/MockDatabase.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.smartcampus.dao;
+
+import com.example.smartcampus.model.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Isith Bhanuka
+ */
+public class MockDatabase {
+    public static final List<Room> ROOM = new ArrayList<>();
+    public static final List<Sensor> SENSOR = new ArrayList<>();
+    public static final List<SensorReading> SENSORREADING = new ArrayList<>();
+}
